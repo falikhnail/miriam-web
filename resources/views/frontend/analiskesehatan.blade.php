f2055676f7e1833a146d213343610b445e9596a0
Falikh Rifqi Nail <68749934+falikhnail@users.noreply.github.com>
2024-04-30 08:32:10 +0700

    update 1 from analis

--- resources/views/frontend/analiskesehatan.blade.php
+++ resources/views/frontend/analiskesehatan.blade.php
@@ -9,18 +9,9 @@
         <div class="container-sm container-md">
             <div class="container-sm container-md my-5">
                 <center><h2 class="fw-bold fs-2">Informasi Lowongan Pekerjaan</h2></center></div>
-            {{-- <div class="my-5 d-flex justify-content-center">
-                <img src="{{ asset('images/karir/bidan-s1.jpeg') }}" alt="">
-            </div>
-            <div class="my-5 d-flex justify-content-center">
-                <img src="{{ asset('images/karir/bidan-d3.jpeg') }}" alt="">
-            </div> --}}
             <div class="my-5 d-flex justify-content-center">
                 <img src="{{ asset('images/karir/ttk.jpg') }}" alt="">
             </div>
-            {{-- <div class="my-5 d-flex justify-content-center">
-                <img src="{{ asset('images/karir/marketing.jpeg') }}" alt="">
-            </div> --}}
         </div>
     </div>
 @endsection
