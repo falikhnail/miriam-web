b061a4ebed7beebc319d9371ce0de90714f8de12
Falikh Rifqi Nail <68749934+falikhnail@users.noreply.github.com>
2024-09-25 10:13:02 +0700

    "Updated image source from 'selasa.jpg' to 'rabu.jpg' in frontend index view."

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -122,7 +122,7 @@
             <div class="col-lg-3 col-md-4 d-flex justify-content-center">
                 <div class="gallery-item position-relative">
                     {{-- <a href="assets/img/meternity.png" class="galelry-lightbox"> --}}
-                        <img src="assets/img/info/selasa.jpg" alt="" class="img-fluid" style="width: 300px; height: auto;">
+                        <img src="assets/img/info/rabu.jpg" alt="" class="img-fluid" style="width: 300px; height: auto;">
                     </a>
                 </div>
             </div>
