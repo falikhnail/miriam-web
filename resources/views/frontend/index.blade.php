f5dd6e51a796a6160ccb772c49cdd06b6439a655
Falikh Rifqi Nail <68749934+falikhnail@users.noreply.github.com>
2024-10-22 08:23:01 +0700

    "Removed two gallery items and updated image source in another"

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -129,20 +129,6 @@
             <div class="col-lg-3 col-md-4 d-flex justify-content-center">
                 <div class="gallery-item position-relative">
                     {{-- <a href="assets/img/meternity.png" class="galelry-lightbox"> --}}
-                        <img src="assets/img/info/bcg21.jpg" alt="" class="img-fluid" style="width: 300px; height: auto;">
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 d-flex justify-content-center">
-                <div class="gallery-item position-relative">
-                    {{-- <a href="assets/img/meternity.png" class="galelry-lightbox"> --}}
-                        <img src="assets/img/info/kuota21.jpg" alt="" class="img-fluid" style="width: 300px; height: auto;">
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 d-flex justify-content-center">
-                <div class="gallery-item position-relative">
-                    {{-- <a href="assets/img/meternity.png" class="galelry-lightbox"> --}}
                         <img src="assets/img/info/herpes.jpg" alt="" class="img-fluid" style="width: 300px; height: auto;">
                     </a>
                 </div>
@@ -150,7 +136,7 @@
             <div class="col-lg-3 col-md-4 d-flex justify-content-center">
                 <div class="gallery-item position-relative">
                     {{-- <a href="assets/img/meternity.png" class="galelry-lightbox"> --}}
-                        <img src="assets/img/info/senin21.jpg" alt="" class="img-fluid" style="width: 300px; height: auto;">
+                        <img src="assets/img/info/selasa22.jpg" alt="" class="img-fluid" style="width: 300px; height: auto;">
                     </a>
                 </div>
             </div>
